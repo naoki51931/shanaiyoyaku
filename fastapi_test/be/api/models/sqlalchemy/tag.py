d50e334c04dd89cd34eca61ef1a15d154c4ff2c9
root <root@DESKTOP-5F6SILT.>
2025-07-11 17:12:04 +0900

    tags add created and updated time

--- fastapi_test/be/api/models/sqlalchemy/tag.py
+++ fastapi_test/be/api/models/sqlalchemy/tag.py
@@ -1,5 +1,5 @@
 # models/sqlalchemy/tag.py
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, func
 from database.database import Base
 from sqlalchemy.orm import relationship
 from models.sqlalchemy.pasokon_tags import pasokon_tags  # 中間テーブルPasokonTagのインポート
@@ -10,6 +10,8 @@ class Tag(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
     # PasokonTag 中間テーブルとの関連を定義
     pasokons = relationship("Pasokon", secondary=pasokon_tags, back_populates="tags")
